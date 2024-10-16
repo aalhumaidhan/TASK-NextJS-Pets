c7d1043b5cf7963f16e8e0d199068f6f0d52c869
Abdullah Alhumaidhan <abdullahkh56@gmail.com>
2024-10-16 14:18:12 +0300

    First commit

--- src/app/Components/PetItem.js
+++ src/app/Components/PetItem.js
@@ -1,17 +1,36 @@
-import Image from "next/image";
+"use client";
 
-const btnStyle = "m-4 p-2 bg-palette-primary text-white rounded-sm font-primary font-semibold  hover:bg-palette-dark"
+import Image from "next/image";
+import { useState } from "react";
+
+const btnStyle =
+  "m-4 p-2 bg-palette-primary text-white rounded-sm font-primary font-semibold  hover:bg-palette-dark";
 
 function PetItem({ pet }) {
+  const [petImage, setPetImage] = useState(pet.image);
+
+  function swapImage() {
+    if (petImage === pet.image) setPetImage(pet.image2);
+    else setPetImage(pet.image);
+  }
+
+  const [isAdopted, setIsAdopted] = useState(false);
+
+  function adoptPet() {
+    setIsAdopted(true);
+  }
+
+  if (isAdopted) return null;
   return (
     <div className="h-120 w-72 rounded shadow-lg mx-auto border border-palette-lighter">
       <div className="h-72 border-b-2 border-palette-lighter relative">
         <Image
           alt={pet.name}
-          src={pet.image}
+          src={petImage}
           className="transform duration-500 ease-in-out hover:brightness-75"
           fill
-          sizes="30vw" />
+          sizes="30vw"
+        />
       </div>
       <div className="h-48 relative">
         <div className="font-primary text-palette-primary text-2xl pt-4 px-4 font-semibold">
@@ -21,10 +40,10 @@ function PetItem({ pet }) {
           {pet.description}
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <button type="button" className={btnStyle}>
+          <button type="button" className={btnStyle} onClick={swapImage}>
             Pet
           </button>
-          <button type="button" className={btnStyle}>
+          <button type="button" className={btnStyle} onClick={adoptPet}>
             Adopt
           </button>
         </div>
